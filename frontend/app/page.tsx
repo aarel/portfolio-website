0a1c40fb118d53a9611b5cff109485fa57b29022
Adam Arellano <7grxt1s@pm.me>
2025-10-21 12:07:05 -0500

    Changed text color to black on 'tech' list for projects on Home page to stand out from gray background

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,12 @@ import ProjectCard from '../components/ProjectCard';
 import { getProjects } from '../lib/api';
 import React = require('react');
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
     const projects = await getProjects();
     return (
-        <main className="min-h-screen bg-gray-50 text-gray-50">
+        <main className="min-h-screen bg-gray-50 text-black">
             <section className="max-w-5xl mx-auto p-8">
                 <h1 className="text-4xl font-bold mb-8">My Projects</h1>
                 <p className="mb-12 text-lg text-gray-600">I build modular AI frameworks and intelligent systems.</p>
